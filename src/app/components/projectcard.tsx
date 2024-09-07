b768d9604c0d77936abf2b73d71546c56c874910
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-07 22:24:55 +0200

    fix: prevent propagation on inside <Link> click

--- src/app/components/projectcard.tsx
+++ src/app/components/projectcard.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import "./projectcard.css";
 import { FaGithub } from "react-icons/fa";
+import { MouseEventHandler } from "react";
 
 type TProjectCard = {
   title: string;
@@ -20,6 +23,12 @@ const ProjectCard: React.FC<TProjectCard> = ({
   title,
 }) => {
   const Tag = external ? `a` : Link;
+
+  const stopPropagation: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+  };
+
   return (
     <Tag
       className="flex flex-col justify-start bg-slate-100 dark:bg-slate-800 transition-colors rounded-xl p-8 relative project-card"
@@ -37,14 +46,17 @@ const ProjectCard: React.FC<TProjectCard> = ({
           />
         </div>
       )}
-      <h3 className="text-l mt-2 mb-2.5 leading-7 font-medium">
-        {title}
-      </h3>
+      <h3 className="text-l mt-2 mb-2.5 leading-7 font-medium">{title}</h3>
       <p className="text-neutral-700 dark:text-neutral-300">{description}</p>
-      <a href={githubRepo} target="_blank">
+      <a
+        href={githubRepo}
+        onClick={stopPropagation}
+        target="_blank"
+        className="absolute bottom-4 right-4"
+      >
         <FaGithub
           fontSize={24}
-          className="project-card-child absolute bottom-4 right-4 hover:opacity-40"
+          className="project-card-child hover:opacity-40"
         />
       </a>
     </Tag>
